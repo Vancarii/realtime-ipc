65bdc9d6d413b791ce85c6278fbc3e849de2c76c
Vancarii <xnycwang@yahoo.com>
2024-02-25 18:05:40 -0800

    working termination

--- makefile
+++ makefile
@@ -1,8 +1,10 @@
 CC = gcc
-CFLAGS = -Wall -Wextra -pthread
+CFLAGS = -Wall -g -std=c99 -D _POSIX_C_SOURCE=200809L -Werror
 
-s-talk: s-talk.c list.c
-	$(CC) $(CFLAGS) s-talk.c -o s-talk
+all: build
+
+build:
+	$(CC) $(CFLAGS) s-talk.c shutdown_manager.c list.c -o s-talk -lpthread
 	
 clean:
 	rm -f s-talk
--- s-talk.c
+++ s-talk.c
@@ -6,77 +6,87 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#include "list.c" 
 #include "list.h"
+#include "shutdown_manager.h"
 
 
-// #define PORT 12345
-// #define BUFFER_SIZE 1024
-
-// Assume List and related functions are implemented
+// lists
+static List *sendList;
+static List *receiveList;
 
 // List sendList;
-pthread_mutex_t sendListMutex;
-pthread_cond_t sendListNotEmptyCond;
+static pthread_mutex_t sendListMutex;
+static pthread_cond_t sendListNotEmptyCond;
 
 // List receiveList;
-pthread_mutex_t receiveListMutex;
-pthread_cond_t receiveListNotEmptyCond;
+static pthread_mutex_t receiveListMutex;
+static pthread_cond_t receiveListNotEmptyCond;
 
 typedef struct {
     char *remoteIP;
     int remotePort;
     int localPort;
-    List *sendList;
-    List *receiveList;
+    // List *sendList;
+    // List *receiveList;
 } thread_args;
 
 void freeList(void* item) {
     free(item);
 }
 
+static char* keyboardMessage = NULL;
+static char* inputMessage = NULL;
 
-void* keyboardInputThread(void* arg) {
+
+void* keyboardInputThread(void*) {
     char inputBuffer[1024]; // Buffer to hold input from the keyboard
 
-    thread_args* arguments = (thread_args*)arg;
+    // thread_args* arguments = (thread_args*)args;
 
-    while (fgets(inputBuffer, sizeof(inputBuffer), stdin) != NULL) {
-        // Remove newline character at the end of input if present
-        inputBuffer[strcspn(inputBuffer, "\n")] = 0;
-
-        // Allocate memory for a new message
-        char* message = strdup(inputBuffer); // Duplicate the string to store in the list
-
-        // Lock the mutex before accessing the shared list
-        pthread_mutex_lock(&sendListMutex);
-
-        //critical section
-
-        // Add the message to the end of sendList 
-        List_append(arguments->sendList, message); 
-
-        // Signal any threads waiting for messages that the list is not empty
-        pthread_cond_signal(&sendListNotEmptyCond);
-
-        // Unlock the mutex after modifying the list
-        pthread_mutex_unlock(&sendListMutex);
-
-        // check if the message is "!"
-        // checks after since we want to send the message before exiting
-        if (strcmp(message, "!") == 0) {
-            printf("Exiting...\n");
-            //free the sendList
-            List_free(arguments->sendList, freeList);
-            //free the receiveList
-            List_free(arguments->receiveList, freeList);
-
-            //exit the program
-            exit(0);
-        }
+    if (sendList == NULL) {
+        printf("sendList is NULL\n");
+        return NULL;
     }
 
-    // Optionally handle cleanup and termination signal here
+    while (1) {
+
+        if (fgets(inputBuffer, sizeof(inputBuffer), stdin) != NULL) {
+
+            // Remove newline character at the end of input if present
+            inputBuffer[strcspn(inputBuffer, "\n")] = 0;
+
+            // Allocate memory for a new message
+            keyboardMessage = strdup(inputBuffer); // Duplicate the string to store in the list
+            if (keyboardMessage == NULL) {
+                perror("strdup");
+                return NULL;
+            }
+
+            // Lock the mutex before accessing the shared list
+            pthread_mutex_lock(&sendListMutex);
+            
+            // Add the message to the end of sendList 
+            List_append(sendList, keyboardMessage); 
+
+            // Signal any threads waiting for messages that the list is not empty
+            pthread_cond_signal(&sendListNotEmptyCond);
+
+        
+            // Unlock the mutex after modifying the list
+            pthread_mutex_unlock(&sendListMutex);
+
+            // waitFor_shutdown();
+
+            if (strcmp(keyboardMessage, "!") == 0){
+                printf("keyboard thread should shutdown...\n");
+                pthread_cond_signal(&sendListNotEmptyCond);
+                pthread_cond_signal(&receiveListNotEmptyCond);
+                signal_shutdown();
+                return NULL;
+            }
+        }
+
+    }
 
     return NULL;
 }
@@ -103,23 +113,40 @@ void* udpOutputThread(void* args) {
     servaddr.sin_port = htons(arguments->remotePort);
 
     while (1) {
-        pthread_mutex_lock(&sendListMutex);
 
+        pthread_mutex_lock(&sendListMutex);
+        
         // Wait for the list to have messages
-        while (arguments->sendList->head == NULL) {
+        while (sendList->head == NULL) {
+
+            if (should_shutdown()){
+                printf("1. output thread should shutdown...\n");
+                return NULL;
+            }
+
             pthread_cond_wait(&sendListNotEmptyCond, &sendListMutex);
         }
 
-
-        char* message = List_trim(arguments->sendList);
-
+        char* message = List_trim(sendList);
+    
         pthread_mutex_unlock(&sendListMutex);
+
 
         // Send the message
         if (message != NULL) {
             sendto(sockfd, message, strlen(message), 0, (const struct sockaddr*)&servaddr, sizeof(servaddr));
             // free(message); // Don't forget to free the memory
+
+            if (should_shutdown()){
+                printf("2. output thread should shutdown...\n");
+                free(message);
+                return NULL;
+            }
+
+
+            free(message);
         }
+
     }
 
     close(sockfd);
@@ -152,25 +179,41 @@ void* udpInputThread(void* args) {
     }
 
     while (1) {
+
         char buffer[1024];
         socklen_t len = sizeof(cliaddr);  //len is value/resuslt
 
         int n = recvfrom(sockfd, (char *)buffer, 1024, 0, ( struct sockaddr *) &cliaddr, &len);
+
         buffer[n] = '\0'; // Null-terminate the received string
 
-        char* message = strdup(buffer); // Duplicate the string to store in the list
+        inputMessage = strdup(buffer); // Duplicate the string to store in the list
+        if (inputMessage == NULL) {
+            perror("strdup");
+            return NULL;
+        }
 
         // Lock the mutex before accessing the shared list
         pthread_mutex_lock(&receiveListMutex);
+        {
+            // Add the received message to the end of receiveList
+            List_append(receiveList, inputMessage); 
 
-        // Add the received message to the end of receiveList
-        List_append(arguments->receiveList, message); 
-
-        // Signal any threads waiting for messages that the list is not empty
-        pthread_cond_signal(&receiveListNotEmptyCond);
-
+            // Signal any threads waiting for messages that the list is not empty
+            pthread_cond_signal(&receiveListNotEmptyCond);
+        }
         // Unlock the mutex after modifying the list
         pthread_mutex_unlock(&receiveListMutex);
+
+
+        if (strcmp(inputMessage, "!") == 0){
+            printf("3. input thread should shutdown...\n");
+            pthread_cond_signal(&sendListNotEmptyCond);
+            pthread_cond_signal(&receiveListNotEmptyCond); 
+            signal_shutdown();
+            return NULL;
+        }
+
     }
 
     close(sockfd);
@@ -179,20 +222,27 @@ void* udpInputThread(void* args) {
 
 void* screenOutputThread(void* args) {
 
-    thread_args* arguments = (thread_args*)args;
+    // thread_args* arguments = (thread_args*)args;
 
     while (1) {
+
         // Lock the mutex before accessing the shared list
         pthread_mutex_lock(&receiveListMutex);
-
+    
         // Wait for the list to have messages
-        while (arguments->receiveList->head == NULL) {
+        while (receiveList->head == NULL) {
+
+            if (should_shutdown()){
+                printf("1. screen thread should shutdown...\n");
+                return NULL;
+            }
+
             pthread_cond_wait(&receiveListNotEmptyCond, &receiveListMutex);
         }
 
         // Assuming list_remove returns a dynamically allocated string
         // that needs to be freed after use
-        char* message = List_trim(arguments->receiveList);
+        char* message = List_trim(receiveList);
 
         // Unlock the mutex after removing the message from the list
         pthread_mutex_unlock(&receiveListMutex);
@@ -200,21 +250,16 @@ void* screenOutputThread(void* args) {
         // Display the message
         if (message != NULL) {
             printf("%s\n", message);
-            // free(message); // Don't forget to free the memory
-        }
 
-        if (strcmp(message, "!") == 0) {
-            printf("Exiting...\n");
-            //free the sendList
-            List_free(arguments->sendList, freeList);
-            //free the receiveList
-            List_free(arguments->receiveList, freeList);
+            if (should_shutdown()){
+                printf("2. screen thread should shutdown...\n");
+                free(message);
+                return NULL;
+            }
 
             free(message);
-            //exit the program
-            exit(0);
         }
-        free(message);
+
     }
 
     return NULL;
@@ -236,6 +281,8 @@ int main(int argc, char *argv[]) {
     printf("Press ! to exit\n");
     printf("start of chat:\n");
 
+    init_shutdown_manager();
+
     pthread_mutex_init(&sendListMutex, NULL);
     pthread_mutex_init(&receiveListMutex, NULL);
     pthread_cond_init(&sendListNotEmptyCond, NULL);
@@ -250,30 +297,51 @@ int main(int argc, char *argv[]) {
 
 
     // Initialize sendList and receiveList
-    args->sendList = List_create();
-    args->receiveList = List_create();
+    // args->sendList = List_create();
+    // args->receiveList = List_create();
+
+    sendList = List_create();
+    receiveList = List_create();
 
 
     // Create threads
-    pthread_create(&threads[0], NULL, keyboardInputThread, args);
+    pthread_create(&threads[0], NULL, keyboardInputThread, NULL);
     pthread_create(&threads[1], NULL, udpOutputThread, args);
     pthread_create(&threads[2], NULL, udpInputThread, args);
     pthread_create(&threads[3], NULL, screenOutputThread, args);
+
+
+    waitFor_shutdown();
+
+    //cleanup
+     
+    pthread_cancel(threads[0]);
+    // pthread_cancel(threads[1]);
+    pthread_cancel(threads[2]);
+    // pthread_cancel(threads[3]);
+
+    printf("Exiting...\n");
+
+    free(keyboardMessage);
+    free(inputMessage);
 
     // Wait for threads to finish
     for (int i = 0; i < 4; i++) {
         pthread_join(threads[i], NULL);
     }
+    
 
-    // Cleanup
+    cleanup_shutdown_manager();
+
     pthread_mutex_destroy(&sendListMutex);
     pthread_mutex_destroy(&receiveListMutex);
     pthread_cond_destroy(&sendListNotEmptyCond);
     pthread_cond_destroy(&receiveListNotEmptyCond);
 
+    List_free(sendList, freeList);
+    List_free(receiveList, freeList);
+
     free(args);
 
-    List_free(args->sendList, freeList);
-    List_free(args->receiveList, freeList);
     return 0;
 }
